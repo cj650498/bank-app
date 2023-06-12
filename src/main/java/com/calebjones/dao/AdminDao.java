9d922fa5f40c655b70a8c9c2b1048c3d675cd929
Caleb Jones <cj650498@icloud.com>
2023-06-12 16:58:04 -0400

    Add AdminDao for database CRUD operations
    The AdminDao class provides methods for creating, retrieving, updating,
    and deleting admin records in the database.

---
+++ src/main/java/com/calebjones/dao/AdminDao.java
@@ -0,0 +1,122 @@
+package com.calebjones.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import com.calebjones.database.DBConnectionManager;
+import com.calebjones.entities.Admin;
+
+public class AdminDao {
+
+    public static void createAdmin(Admin admin) {
+        try (Connection connection = DBConnectionManager.getConnection()) {
+            String query = "INSERT INTO Admin (name, email, password, address) VALUES (?, ?, ?, ?)";
+
+            try (PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+
+                statement.setString(1, admin.getName());
+                statement.setString(2, admin.getEmail());
+                statement.setString(3, admin.getPassword());
+                statement.setString(4, admin.getAddress());
+
+                statement.executeUpdate();
+
+                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int adminId = generatedKeys.getInt(1);
+                        admin.setAdminId(adminId);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } // END: createAdmin
+
+    public static Admin getAdminById(int adminId) {
+        Admin admin = null;
+
+        try (Connection connection = DBConnectionManager.getConnection()) {
+            String query = "SELECT * FROM Admin WHERE admin_id = ?";
+
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setInt(1, adminId);
+
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    if (resultSet.next()) {
+                        admin = new Admin();
+                        admin.setAdminId(resultSet.getInt("admin_id"));
+                        admin.setName(resultSet.getString("name"));
+                        admin.setEmail(resultSet.getString("email"));
+                        admin.setPassword(resultSet.getString("password"));
+                        admin.setAddress(resultSet.getString("address"));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return admin;
+    } // END: getAdminById
+
+    public static void updateAdmin(Admin admin) {
+        try (Connection connection = DBConnectionManager.getConnection()) {
+            String query = "UPDATE Admin SET name = ?, email = ?, password = ?, address = ? WHERE admin_id = ?";
+
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setString(1, admin.getName());
+                statement.setString(2, admin.getEmail());
+                statement.setString(3, admin.getPassword());
+                statement.setString(4, admin.getAddress());
+                statement.setInt(5, admin.getAdminId());
+
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } // END: updateAdmin
+
+    public static void deleteAdmin(int adminId) {
+        try (Connection connection = DBConnectionManager.getConnection()) {
+            String query = "DELETE FROM Admin WHERE admin_id = ?";
+
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setInt(1, adminId);
+
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } // END: deleteAdmin
+
+    public static List<Admin> getAllAdmins() {
+        List<Admin> admins = new ArrayList<>();
+
+        try (Connection connection = DBConnectionManager.getConnection();
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM Admin");
+                ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                Admin admin = new Admin();
+                admin.setAdminId(resultSet.getInt("admin_id"));
+                admin.setName(resultSet.getString("name"));
+                admin.setEmail(resultSet.getString("email"));
+                admin.setPassword(resultSet.getString("password"));
+                admin.setAddress(resultSet.getString("address"));
+
+                admins.add(admin);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return admins;
+    } // END: getAllAdmins
+
+} // END CLASS
